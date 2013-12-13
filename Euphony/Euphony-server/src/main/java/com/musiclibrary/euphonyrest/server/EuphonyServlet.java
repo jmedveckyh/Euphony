169f607779b4544364acdc1515df04909d768216
PC <PC@Brano>
2013-12-13 16:27:30 +0100

    branno(PC): server get

--- Euphony/Euphony-server/src/main/java/com/musiclibrary/euphonyrest/server/EuphonyServlet.java
+++ Euphony/Euphony-server/src/main/java/com/musiclibrary/euphonyrest/server/EuphonyServlet.java
@@ -5,13 +5,11 @@
 package com.musiclibrary.euphonyrest.server;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.musiclibrary.euphonyapi.dto.ArtistDTO;
+import com.musiclibrary.euphonyapi.dto.GenreDTO;
 import com.musiclibrary.euphonyapi.services.ArtistService;
 import com.musiclibrary.euphonyapi.services.GenreService;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.ResourceBundle;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
@@ -49,21 +47,63 @@ public class EuphonyServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String pathInfo = request.getPathInfo();
 
-        if (artistService == null) {
-            response.getOutputStream().print("NEFUNGUJE");
-        } else {
-            response.getOutputStream().print("FUNGUJE!!!");
+        /*if (artistService == null) {
+         response.getOutputStream().print("NEFUNGUJE");
+         } else {
+         response.getOutputStream().print("FUNGUJE!!!");
+         }
+         */
+        if (pathInfo != null) {
+            if (pathInfo.matches("/")) {
+                RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
+                dispatcher.forward(request, response);
+            }
+            if (pathInfo.matches("/artists")) {
+                //response.getOutputStream().print("artists ");
+                response.setContentType("application/json");
+                ObjectMapper mapper = new ObjectMapper();
+                mapper.writeValue(response.getOutputStream(), artistService.getAll());
+            } else if (pathInfo.matches("/genres")) {
+                //response.getOutputStream().print("genres ");
+                response.setContentType("application/json");
+                ObjectMapper mapper = new ObjectMapper();
+                mapper.writeValue(response.getOutputStream(), genreService.getAll());
+            } else if (pathInfo.matches("/artist")) {
+                //response.getOutputStream().print("artist ");
+                response.setContentType("application/json");
+                Long id = Long.parseLong(request.getParameter("id"));
+                if (id != null) {
+                    ArtistDTO artist = artistService.getById(id);
+                    if (artist != null) {
+                        ObjectMapper mapper = new ObjectMapper();
+                        mapper.writeValue(response.getOutputStream(), artist);
+                    } else {
+                        response.sendError(404, properties.getString("error.nosuchartist")); 
+                    }
+                }
+            } else if (pathInfo.matches("/genre")) {
+                //response.getOutputStream().print("genre ");
+                response.setContentType("application/json");
+                Long id = Long.parseLong(request.getParameter("id"));
+                if (id != null) {
+                    GenreDTO genre = genreService.getById(id);
+                    if (genre != null) {
+                        ObjectMapper mapper = new ObjectMapper();
+                        mapper.writeValue(response.getOutputStream(), genre);
+                    } else {
+                        response.sendError(404, properties.getString("error.nosuchgenre"));
+                    }
+                }
+            }
         }
 
     }
 
     @Override
     protected synchronized void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
     }
 }
