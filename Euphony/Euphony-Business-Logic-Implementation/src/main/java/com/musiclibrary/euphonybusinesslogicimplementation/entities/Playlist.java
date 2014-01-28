7d7788da0360e25b5fa2a007719fde5aabbd147f
tomillie <tomizzi@tomizzi.com>
2014-01-28 03:20:14 +0100

    removing playlist bug fix

--- Euphony/Euphony-Business-Logic-Implementation/src/main/java/com/musiclibrary/euphonybusinesslogicimplementation/entities/Playlist.java
+++ Euphony/Euphony-Business-Logic-Implementation/src/main/java/com/musiclibrary/euphonybusinesslogicimplementation/entities/Playlist.java
@@ -21,7 +21,7 @@ public class Playlist implements Serializable {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany
     private Map<Integer, Song> songs;
 
     public Playlist() {
